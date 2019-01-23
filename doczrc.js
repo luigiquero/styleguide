79bdf593ba9d382eeaea5e1230bf89b5e0c73aa6
Felipe Orlando <fobsouza@gmail.com>
2019-01-23 12:36:17 -0200

    Add docz config file

---
+++ doczrc.js
@@ -0,0 +1,15 @@
+export default {
+  title: 'Indeva Styleguide',
+  description: 'The UI library by Indeva',
+  repository: 'https://github.com/indeva/styleguide',
+  ordering: 'ascending',
+  menu: [
+    'Home',
+  ],
+  themeConfig: {
+    colors: {
+      primary: '#f47d35',
+      link: '#2463d0'
+    }
+  }
+}
