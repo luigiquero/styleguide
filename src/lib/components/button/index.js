ffb014a268dcb6b58d7f4198547fdca0ef65a0d9
Felipe Orlando <fobsouza@gmail.com>
2019-01-25 15:08:41 -0200

    Create button component

--- src/lib/components/button/index.js
+++ src/lib/components/button/index.js
@@ -1,3 +1,18 @@
-import Button from '@vtex/styleguide/lib/Button/index';
+import React from 'react';
+import PropTypes from 'prop-types';
+import './button.scss';
+
+const Button = ({ children, mod }) => (
+  <button className={`button ${mod}`}>{ children }</button>
+);
+
+Button.defaultProps = {
+  mod: '',
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  mod: PropTypes.oneOf(['button--primary']),
+};
 
 export default Button;
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,8 @@
+import 'vtex-tachyons';
+import './styles/main.scss';
+
 import Button from './components/button';
 
 export {
-  Button
-}
+  Button,
+};
