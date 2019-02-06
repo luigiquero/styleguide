cad58a3eb9cd8fcd88920a17142f1361ca81509c
Felipe Orlando <fobsouza@gmail.com>
2019-02-05 22:29:35 -0200

    Export switch on lib

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,8 +2,10 @@ import './styles/main.scss';
 
 import Button from './components/button';
 import Menu from './components/menu';
+import Switch from './components/form/switch';
 
 export {
   Button,
   Menu,
+  Switch,
 };
