9e70f100e946e6d033f656310c5dd449fae0afd0
Felipe Orlando <fobsouza@gmail.com>
2019-02-05 22:29:24 -0200

    Create switch controlled component to use on doc

---
+++ src/docs/components/form/switch-controlled.js
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react';
+import { Switch as SimpleSwitch } from '../../../lib';
+
+export default class Switch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checked: props.checked,
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange() {
+    const { checked } = this.state;
+
+    this.setState({ checked: !checked });
+  }
+
+  render() {
+    const { checked } = this.state;
+
+    return (
+      <SimpleSwitch
+        {...this.props}
+        checked={checked}
+        onChange={this.onChange}
+      />
+    );
+  }
+}
