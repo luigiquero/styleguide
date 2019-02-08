99ad165ba15ff528c22712f19b996427b24fe9df
Jessé Cirilo Soares <jessecsoares@gmail.com>
2019-02-08 18:30:27 -0200

    Enable customize class at Card component

--- src/lib/components/card/index.js
+++ src/lib/components/card/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './card.scss';
 
-const Card = ({ children }) => (
-  <div className="card">{children}</div>
-);
+const Card = ({ className, children }) => {
+  const classes = classNames('card', className);
+
+  return (
+    <div className={classes}>{children}</div>
+  );
+};
+
+Card.defaultProps = {
+  className: null,
+};
 
 Card.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
 
 export default Card;
