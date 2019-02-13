70245078eedb57927ec215e9f63c14f78465e5ec
Jessé Cirilo Soares <jessecsoares@gmail.com>
2019-02-13 17:10:03 -0200

    Add featured card modifier to add extra paddings

--- src/lib/components/card/__tests__/card.test.js
+++ src/lib/components/card/__tests__/card.test.js
@@ -9,6 +9,14 @@ describe('Card', () => {
     expect(tree).toMatchSnapshot();
   });
 
+  describe('with featured modifier', () => {
+    it('should render card properly', () => {
+      const component = renderer.create(<Card featured>small link</Card>);
+      const tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
   describe('whith custom class', () => {
     it('should render card properly', () => {
       const component = renderer.create(<Card className="my classes">my card</Card>);
--- src/lib/components/card/index.js
+++ src/lib/components/card/index.js
@@ -3,8 +3,13 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './card.scss';
 
-const Card = ({ className, children }) => {
-  const classes = classNames('card', className);
+const Card = ({ className, children, featured }) => {
+  const classes = classNames(
+    'card', className,
+    {
+      'card--featured': featured,
+    },
+  );
 
   return (
     <div className={classes}>{children}</div>
@@ -13,11 +18,13 @@ const Card = ({ className, children }) => {
 
 Card.defaultProps = {
   className: null,
+  featured: false,
 };
 
 Card.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  featured: PropTypes.bool,
 };
 
 export default Card;
