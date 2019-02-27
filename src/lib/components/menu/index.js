c3de98519df99bc51da7caa8eeb46249e64e1a68
Felipe Orlando <fobsouza@gmail.com>
2019-02-27 12:04:13 -0300

    Document all menu links

--- src/lib/components/menu/index.js
+++ src/lib/components/menu/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import Link from './link';
 
-const Menu = () => (
-  <div>
-    <div><Link>Link</Link></div>
-    <div><Link active>Link Active</Link></div>
-    <div><Link url="/">Link with URL</Link></div>
-    <div><Link child>Link Child</Link></div>
-    <div><Link child active>Link Child Active</Link></div>
-  </div>
-);
+const Menu = () => null;
 
 export default Menu;
