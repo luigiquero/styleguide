e03b04eb60e0ce3efa987a30df4a0f2c6238b9ec
Felipe Orlando <fobsouza@gmail.com>
2019-03-08 15:57:02 -0300

    Create Menu, with properly animations and minified version

--- src/lib/components/menu/index.js
+++ src/lib/components/menu/index.js
@@ -1,6 +1,32 @@
-// import React from 'react';
-// import Link from './link';
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Item from './item';
+import './menu.scss';
 
-const Menu = () => null;
+const Menu = ({
+  minified,
+  links,
+}) => {
+  const className = classNames(
+    'menu',
+    { 'menu--minified': minified },
+  );
+
+  return (
+    <div className={className}>
+      <ul className="menu__nav">
+        { links.map(linkProps => <Item {...linkProps} />) }
+      </ul>
+    </div>
+  );
+};
+
+Menu.propTypes = {
+  minified: PropTypes.bool.isRequired,
+  links: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default Menu;
