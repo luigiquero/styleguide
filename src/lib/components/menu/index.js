ec99cbb6ad3cb56a4a528363d84a664b465763ef
Felipe Orlando <fobsouza@gmail.com>
2019-03-11 18:06:26 -0300

    Add logo and burguer icon properly

--- src/lib/components/menu/index.js
+++ src/lib/components/menu/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Item from './item';
+import LogoIn from './logo-in.svg';
+import LogoDeva from './logo-deva.svg';
+import BurguerIcon from './burguer-icon.svg';
 import './menu.scss';
 
 const Menu = ({
@@ -15,6 +18,14 @@ const Menu = ({
 
   return (
     <div className={className}>
+      <div className="menu__header">
+        <div className="menu__logo-wrapper">
+          <img src={LogoIn} className="menu__logo menu__logo--in" />
+          <img src={LogoDeva} className="menu__logo menu__logo--deva" />
+        </div>
+        <img src={BurguerIcon} alt="Minificar menu" className="menu__burguer" />
+      </div>
+
       <ul className="menu__nav">
         { links.map(linkProps => <Item {...linkProps} />) }
       </ul>
