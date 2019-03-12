aa86028e46b9600ccb33d90644670263b7bb9895
Felipe Orlando <fobsouza@gmail.com>
2019-03-12 15:55:00 -0300

    Add missing icons

--- src/lib/icons.js
+++ src/lib/icons.js
@@ -1,20 +1,35 @@
 /* eslint-disable no-unused-vars */
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import {
-  faStore as fasStore,
   faAnalytics as fasAnalytics,
-  faTrophy as fasTrophy,
   faCheckCircle as fasCheckCircle,
+  faStore as fasStore,
+  faTrophy as fasTrophy,
 } from '@fortawesome/pro-solid-svg-icons';
-import { faCheck as farCheck } from '@fortawesome/pro-regular-svg-icons';
+
+import {
+  faCheck as farCheck,
+  faCheckCircle as farCheckCircle,
+  faExclamationTriangle as farExclamationTriangle,
+  faTimes as farTimes,
+  faTimesCircle as farTimesCircle,
+} from '@fortawesome/pro-regular-svg-icons';
+
+import { faIgloo as falIgloo } from '@fortawesome/pro-light-svg-icons';
 
 library.add(
-  fasStore,
   fasAnalytics,
-  fasTrophy,
   fasCheckCircle,
+  fasStore,
+  fasTrophy,
   farCheck,
+  farCheckCircle,
+  farExclamationTriangle,
+  farTimes,
+  farTimesCircle,
+  falIgloo,
 );
 
 export default FontAwesomeIcon;
