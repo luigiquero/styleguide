7d7a80d07f1ba9c2e11c698efdfb28f84fc99cfc
Felipe Orlando <fobsouza@gmail.com>
2019-03-12 11:35:38 -0300

    Create statefull menu

--- src/lib/components/menu/index.js
+++ src/lib/components/menu/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Logo from './logo';
@@ -6,31 +6,45 @@ import Item from './item';
 import BurguerIcon from './burguer-icon.svg';
 import './menu.scss';
 
-const Menu = ({
-  minified,
-  links,
-}) => {
-  const className = classNames(
-    'menu',
-    { 'menu--minified': minified },
-  );
+class Menu extends Component {
+  state = {
+    minified: false,
+  };
 
-  return (
-    <div className={className}>
-      <div className="menu__header">
-        <Logo />
-        <img src={BurguerIcon} alt="Minificar menu" className="menu__burguer" />
+  toggleMinified = () => {
+    const { minified } = this.state;
+    this.setState(() => ({ minified: !minified }));
+  }
+
+  render() {
+    const { minified } = this.state;
+    const { links } = this.props;
+
+    const className = classNames(
+      'menu',
+      { 'menu--minified': minified },
+    );
+
+    return (
+      <div className={className}>
+        <div
+          className="menu__header"
+          role="presentation"
+          onClick={this.toggleMinified}
+        >
+          <Logo />
+          <img src={BurguerIcon} alt="Minificar menu" className="menu__burguer" />
+        </div>
+
+        <ul className="menu__nav">
+          { links.map(linkProps => <Item {...linkProps} />) }
+        </ul>
       </div>
-
-      <ul className="menu__nav">
-        { links.map(linkProps => <Item {...linkProps} />) }
-      </ul>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Menu.propTypes = {
-  minified: PropTypes.bool.isRequired,
   links: PropTypes.arrayOf(
     PropTypes.object,
   ).isRequired,
