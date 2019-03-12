d29e74812e8ecadb421ebcc0257f2b45a1405615
Felipe Orlando <fobsouza@gmail.com>
2019-03-12 15:56:49 -0300

    Export lib directluy from src

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import './lib/styles/main.scss';
+import Alert from './lib/components/alert';
+import Button from './lib/components/button';
+import Card from './lib/components/card';
+import FormGroup from './lib/components/form/form-group';
+import Label from './lib/components/label';
+import LinedInput from './lib/components/form/lined-input';
+import LinedInputGroup from './lib/components/form/lined-input-group';
+import Link from './lib/components/link';
+import Menu from './lib/components/menu';
+import Switch from './lib/components/form/switch';
+
+export {
+  Alert,
+  Button,
+  Card,
+  FormGroup,
+  Label,
+  LinedInput,
+  LinedInputGroup,
+  Link,
+  Menu,
+  Switch,
+};
