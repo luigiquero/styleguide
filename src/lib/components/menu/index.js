c0cdcecdc8ec4f21b8d7cb1de26874ccdd557fe4
Felipe Orlando <fobsouza@gmail.com>
2019-03-12 10:47:59 -0300

    Separate logo from menu

--- src/lib/components/menu/index.js
+++ src/lib/components/menu/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Logo from './logo';
 import Item from './item';
-import LogoIn from './logo-in.svg';
-import LogoDeva from './logo-deva.svg';
 import BurguerIcon from './burguer-icon.svg';
 import './menu.scss';
 
@@ -19,10 +18,7 @@ const Menu = ({
   return (
     <div className={className}>
       <div className="menu__header">
-        <div className="menu__logo-wrapper">
-          <img src={LogoIn} className="menu__logo menu__logo--in" />
-          <img src={LogoDeva} className="menu__logo menu__logo--deva" />
-        </div>
+        <Logo />
         <img src={BurguerIcon} alt="Minificar menu" className="menu__burguer" />
       </div>
 
