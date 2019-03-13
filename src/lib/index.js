923ec79a2c5d03665157073307971514a3f6e21e
Felipe Orlando <fobsouza@gmail.com>
2019-03-13 17:39:16 -0300

    Move all to lib again and import to it

---
+++ src/lib/index.js
@@ -0,0 +1,24 @@
+import './styles/main.scss';
+import Alert from './components/alert';
+import Button from './components/button';
+import Card from './components/card';
+import FormGroup from './components/form/form-group';
+import Label from './components/label';
+import LinedInput from './components/form/lined-input';
+import LinedInputGroup from './components/form/lined-input-group';
+import Link from './components/link';
+import Menu from './components/menu';
+import Switch from './components/form/switch';
+
+export {
+  Alert,
+  Button,
+  Card,
+  FormGroup,
+  Label,
+  LinedInput,
+  LinedInputGroup,
+  Link,
+  Menu,
+  Switch,
+};
