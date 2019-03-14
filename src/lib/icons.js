87a665da83ae2212e61ed7ef92f4b0eebe1453c5
Felipe Orlando <fobsouza@gmail.com>
2019-03-14 16:09:37 -0300

    Import icons properly

--- src/lib/icons.js
+++ src/lib/icons.js
@@ -3,20 +3,20 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // Pro Solid
-import fasAnalytics from '@fortawesome/pro-solid-svg-icons/faAnalytics';
-import fasCheckCircle from '@fortawesome/pro-solid-svg-icons/faCheckCircle';
-import fasStore from '@fortawesome/pro-solid-svg-icons/faStore';
-import fasTrophy from '@fortawesome/pro-solid-svg-icons/faTrophy';
+import { faAnalytics as fasAnalytics } from '@fortawesome/pro-solid-svg-icons/faAnalytics';
+import { faCheckCircle as fasCheckCircle } from '@fortawesome/pro-solid-svg-icons/faCheckCircle';
+import { faStore as fasStore } from '@fortawesome/pro-solid-svg-icons/faStore';
+import { faTrophy as fasTrophy } from '@fortawesome/pro-solid-svg-icons/faTrophy';
 
 // Pro Regular
-import farCheck from '@fortawesome/pro-regular-svg-icons/faCheck';
-import farCheckCircle from '@fortawesome/pro-regular-svg-icons/faCheckCircle';
-import farExclamationTriangle from '@fortawesome/pro-regular-svg-icons/faExclamationTriangle';
-import farTimes from '@fortawesome/pro-regular-svg-icons/faTimes';
-import farTimesCircle from '@fortawesome/pro-regular-svg-icons/faTimesCircle';
+import { faCheck as farCheck } from '@fortawesome/pro-regular-svg-icons/faCheck';
+import { faCheckCircle as farCheckCircle } from '@fortawesome/pro-regular-svg-icons/faCheckCircle';
+import { faExclamationTriangle as farExclamationTriangle } from '@fortawesome/pro-regular-svg-icons/faExclamationTriangle';
+import { faTimes as farTimes } from '@fortawesome/pro-regular-svg-icons/faTimes';
+import { faTimesCircle as farTimesCircle } from '@fortawesome/pro-regular-svg-icons/faTimesCircle';
 
 // Pro Light
-import falIgloo from '@fortawesome/pro-light-svg-icons/faIgloo';
+import { faIgloo as falIgloo } from '@fortawesome/pro-light-svg-icons/faIgloo';
 
 library.add(
   fasAnalytics,
