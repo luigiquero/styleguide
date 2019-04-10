4f20cd0f6f937b45228f1f83d78c7788623f3e3d
Felipe Orlando <fobsouza@gmail.com>
2019-04-10 10:48:46 -0300

    Move logo to topar

--- src/lib/components/menu/index.js
+++ src/lib/components/menu/index.js
@@ -2,9 +2,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Logo from './logo';
 import Item from './item';
-import BurguerIcon from './burguer-icon.svg';
 import './menu.scss';
 
 class Menu extends Component {
@@ -30,19 +28,6 @@ class Menu extends Component {
 
     return (
       <div className={classes}>
-        <div
-          className="menu__header"
-          role="presentation"
-          onClick={this.toggleMinified}
-        >
-          <Logo />
-          <img
-            src={BurguerIcon}
-            alt="Minificar menu"
-            className="menu__burguer"
-          />
-        </div>
-
         <ul className="menu__nav">
           {
             menuItems.map(({
