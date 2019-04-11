c74fde6699c0ca99e6986a21d7424750302019cd
Felipe Orlando <fobsouza@gmail.com>
2019-04-11 09:56:53 -0300

    Add minified and not minified case for Logo

--- src/lib/components/topbar/logo/__tests__/logo.test.js
+++ src/lib/components/topbar/logo/__tests__/logo.test.js
@@ -3,8 +3,14 @@ import renderer from 'react-test-renderer';
 import Logo from '..';
 
 describe('Logo menu component', () => {
-  it('should render properly', () => {
-    const component = renderer.create(<Logo />);
+  it('should render properly when is not minified', () => {
+    const component = renderer.create(<Logo minified={false} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render properly when is minified', () => {
+    const component = renderer.create(<Logo minified />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
